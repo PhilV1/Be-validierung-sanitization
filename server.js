a52d3d2b3126f7329c115c46308a4f7488e4843a
PhilV1 <werpub.phil@gmx.de>
2024-05-29 11:52:54 +0200

    chore: Update npm dependencies for cors and validator

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import connectDB from './database/connectDB.js'
+import User from './models/userModel.js'
+
 dotenv.config()
 
 const app = express()
-
-app.use(cors())
-app.use(express.json())
-
 const port = process.env.PORT || 5060
-
 const startServer = async () => {
   try {
     await connectDB(process.env.MONGO_URL)
@@ -22,5 +19,42 @@ const startServer = async () => {
     console.log(error)
   }
 }
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// routes
+app.post('/register', async (req, res) => {
+  const { email, password, biography, username } = req.body
+
+  if (!email || !password || !biography || !username) {
+    return res.status(400).json({ error: 'Invalid registration' })
+  }
+
+  try {
+    const user = await User.create({ biography, email, password, username })
+    res.status(201).json(user)
+  } catch (error) {
+    res.status(500).json({ error: 'Error creating user' })
+  }
+})
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Invalid login' })
+  }
+
+  const user = await User.findOne({ email })
+  if (!user) {
+    return res.status(401).json({ error: 'Invalid login' })
+  }
+
+  res.json({ status: 'success ', user })
+})
+
+app.get('/users', async (req, res) => res.json(await User.find()))
 
 startServer()
